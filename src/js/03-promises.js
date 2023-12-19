143b108f784870a3ff9fea00dbb8d4684064c9f2
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-19 12:09:44 +0100

    update task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+const btnSubmit = document.querySelector('.form button');
+form.addEventListener('submit', hadleSubmit);
+
+function hadleSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.target.elements;
+  let j = +delay.value;
+
+  if (+delay.value < 0 || +step.value < 0 || +amount.value <= 0) {
+    Notify.failure('Error', 'Please enter a value of 0 or greater');
   } else {
-    // Reject
+    btnSubmit.setAttribute('disabled', 'disabled');
+    setTimeout(() => {
+      btnSubmit.removeAttribute('disabled');
+    }, +delay.value + +step.value * (+amount.value - 1));
+    for (let i = 1; i <= +amount.value; i++) {
+      createPromise(i, j)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+            timeout: 5000,
+          });
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+            timeout: 5000,
+          });
+        });
+      j += +step.value;
+    }
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
